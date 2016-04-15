5e3e9adf71ac550b8b198ab485ca9b57223a71f1
kbl <klsstt@qq.com>
2016-04-15 20:11:57 +0800

    Update script.js

--- source/js/script.js
+++ source/js/script.js
@@ -134,3 +134,12 @@ function afterPjax() {
   container.scroll(check);
 }afterPjax();
 
+var duoshuoQuery = {short_name:"klsstt"};
+	(function() {
+		var ds = document.createElement('script');
+		ds.type = 'text/javascript';ds.async = true;
+		ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+		ds.charset = 'UTF-8';
+		(document.getElementsByTagName('head')[0] 
+		 || document.getElementsByTagName('body')[0]).appendChild(ds);
+	})();
